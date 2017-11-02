25f62f046cd00d954ac1cdd091bd40e1c8b10018
deddy <32954228+deddythok@users.noreply.github.com>
2017-11-02 16:46:39 +0700

    first attempt

---
+++ script.js
@@ -0,0 +1,31 @@
+// functions constructor
+
+// var uwais = {
+// 	name : 'Uwais',
+// 	yearOfBirth: 1990,
+// 	job: 'programmer'
+// };
+
+var Person = function(name, yearOfBirth,job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+		console.log(2017 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Keren';
+
+var uwais = new Person('Uwais', 2012, 'programmer');
+var wafaa = new Person('Wafaa', 2009,'programmer juga');
+var sarah = new Person('Sarah', 2015, 'dokter');
+
+uwais.calculateAge();
+wafaa.calculateAge();
+sarah.calculateAge();
+
+console.log(uwais.lastName);
+console.log(wafaa.lastName);
+console.log(sarah.lastName);
