2b8da73d97fbee397fde180b110d346e5782f2e3
Deddy thok <deddy.donchild@gmail.com>
2017-11-02 17:33:56 +0700

    ok

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ var Person = function(name, yearOfBirth,job) {
 	this.job = job;
 }
 
+var Mobil = function(merk, warna, tahunPembuatan) {
+	this.merk = merk;
+	this.warna = warna;
+	this.tahunPembuatan = tahunPembuatan;
+}
+
+Mobil.prototype.penggunaan = function() {
+	console.log(2017 - this.tahunPembuatan);
+};
+
 Person.prototype.calculateAge = function() {
 		console.log(2017 - this.yearOfBirth);
 };
@@ -15,6 +25,11 @@ var uwais = new Person('Uwais', 2012, 'programmer');
 var wafaa = new Person('Wafaa', 2009,'programmer juga');
 var sarah = new Person('Sarah', 2015, 'dokter');
 
+var ferarri = new Mobil('ferarri', 'merah', 2015);
+
+console.log(ferarri);
+ferarri.penggunaan();
+
 uwais.calculateAge();
 wafaa.calculateAge();
 sarah.calculateAge();
@@ -22,3 +37,6 @@ sarah.calculateAge();
 console.log(uwais.lastName);
 console.log(wafaa.lastName);
 console.log(sarah.lastName);
+
+
+
