7633ab3889ab4d1f478b9f43f2914f6551a8d1b5
Deddy thok <deddy.donchild@gmail.com>
2017-11-03 11:07:13 +0700

    ok

--- script.js
+++ script.js
@@ -82,3 +82,70 @@ console.log(obj2.age);
 
 
 // Passing functions as arguments
+
+/*
+var years = [1990,1965, 1937,2005, 1998];
+
+function arrayCalc(arr, fn) {
+	var arrRes=[];
+	for(var i = 0; i < arr.length;i++ ){
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+function calculateAge(el) {
+	return 2017 - el;
+}
+
+function isFullAge(el) {
+	return el >= 18;
+}
+
+function maxHeartRate(el) {
+	if(el >= 18 && el <= 81) {
+		return Math.round(206.9 - (0.67 * el));
+	} else {
+		return -1;
+	}
+	
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages,isFullAge); 
+var rates = arrayCalc(ages, maxHeartRate );
+var rates = arrayCalc(ages,maxHeartRate);
+console.log(ages);
+console.log(fullAges);
+
+*/
+
+//functions returning functions
+/*
+function interviewQuestion(job) {
+	if(job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you please explain what UX desing is?');
+		}
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log('What subject do you teach, ' + name +'?');
+		}
+	} else {
+		return function(name) {
+			console.log('Hello ' + name +' what do you do?');
+		}
+	}
+}
+
+
+var teacherQuestion = interviewQuestion('teacher');
+
+teacherQuestion('Uwais');
+
+var designerQuestion = interviewQuestion('designer');
+
+designerQuestion('sarah');
+
+interviewQuestion('teacher')('wafaa');
+*/
